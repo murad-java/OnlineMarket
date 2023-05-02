fb63895ea682f992a34babe6b8ee338d1ff15785
M-U-R-A-D <xakus22@mail.ru>
2023-05-02 04:16:33 +0400

    Add Bash file

---
+++ EurekaService/start.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+PROJECT_NAME=EurekaService
+
+# Определить директорию проекта
+PROJECT_DIR=/home/ubuntu/mymarket/OnlineMarket/${PROJECT_NAME}
+
+# Перейти в директорию проекта
+cd ${PROJECT_DIR}
+
+# Собрать проект с помощью Maven
+mvn clean package
+
+# Определить путь к файлу JAR
+JAR_FILE=target/${PROJECT_NAME}-0.0.1-SNAPSHOT.jar.jar
+
+# Определить имя сервиса
+SERVICE_NAME=${PROJECT_NAME}
+
+# Определить путь к файлу конфигурации
+#SERVICE_CONFIG=${PROJECT_DIR}/myapp.conf
+
+# Создать службу systemd для сервиса
+echo "[Unit]
+Description=My Java Spring Boot App
+After=syslog.target
+
+[Service]
+User=root
+ExecStart=/usr/bin/java -jar ${JAR_FILE}
+SuccessExitStatus=143
+Restart=always
+
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/${SERVICE_NAME}.service
+
+# Обновить службу systemd и запустить сервис
+systemctl daemon-reload
+systemctl start ${SERVICE_NAME}
+systemctl enable ${SERVICE_NAME}
